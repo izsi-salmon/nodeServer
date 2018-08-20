111486811788be963dc309cd7dbf3427c10664af
isabelle may salmon <845868806@bun2-206-13.local>
2018-08-21 09:55:03 +1200

    url requests

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 const http = require('http');
 
 var server = http.createServer(function(request, response){
-  response.writeHead(200, {'content-Type':'text/plain'});
-  response.end('This is running from a server, we installed nodemon.');
+  // console.log(request);
+  var page;
+  if(request.url === '/'){
+    page = 'home';
+  } else if(request.url === '/contact'){
+    page = 'contact';
+  } else if(request.url === '/about'){
+    page = 'about';
+  } else {
+    page = '404 page not found';
+  }
+
+  response.writeHead(200, {'content-Type':'text/html'});
+  response.end(`
+      <html>
+        <head>
+          <title>Node Server</title>
+        </head>
+        <body>
+          <h1 style="color:green">${page}</h1>
+          <p>${request.url}</p>
+          <p>${request.method}</p>
+        </body>
+      </html>
+    `);
 });
 // Port number may need to be changed depending on computer
 server.listen(3000);
